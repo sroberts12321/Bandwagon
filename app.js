371f9e535b922ae2e94966744e28e2a496d25024
Stephen Roberts <Sroberts12321@gmail.com>
2018-12-06 12:34:08 -0600

    starting revisions for bandwagon

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('/index')
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
